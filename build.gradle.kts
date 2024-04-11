0dbdab48c8fe7e957ad433c9d43b221d87583c07
Alexandre DURANDET <a.durandet@gmail.com>
2024-04-11 18:33:07 +0200

    sonar Analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,13 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "adurandet_myapplication")
+        property("sonar.organization", "adurandet")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
